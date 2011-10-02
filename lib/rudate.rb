933088e632d092370dab852d3e845e3d13867027
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-02 15:40:09 +0400

    v 0.0.2 working code

--- lib/rudate.rb
+++ lib/rudate.rb
@@ -1,8 +1,17 @@
 require "rudate/version"
 
 module Rudate
-  def self.hello
-    puts "hello"
+  def self.to_s(year, month, day)
+    month_names = %w{nil Января Февраля Марта Апреля Мая Июня Июля Августа Сентября Октября Ноября Декабря}
+    day.to_s + " " + month_names[month].to_s + " " + year.to_s + "г"
   end
+
+  def self.to_s(tm)
+    day = tm.strftime("%d").to_i
+    month = tm.strftime("%m").to_i
+    year = tm.strftime("%Y").to_i
+    Dateru.to_s(year,month,day)
+  end
+
 
 end
--- lib/rudate/version.rb
+++ lib/rudate/version.rb
@@ -1,3 +1,3 @@
 module Rudate
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
